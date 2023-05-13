82619f5fd2f16eece378ea8a1636b77ddcc67c4b
speedstarkiwi <121459772+speedstarkiwi@users.noreply.github.com>
2023-05-13 17:41:29 -0400

    Create mem alloc.cpp

---
+++ mem alloc.cpp
@@ -0,0 +1,8 @@
+while (true) { // pure hell do not EVEN loop (speed, I know you quit cus of byfron, BUT NOW THIS!??)
+    int* RAM = new int; // allocate memory int
+    int* RAM2 = new int; // if you have 2 RAM sticks, add this :kek:
+    
+    // this technically freez the mem but if you wanna make some dangerous code then remove it
+    delete RAM; 
+    delete RAM2;
+}
